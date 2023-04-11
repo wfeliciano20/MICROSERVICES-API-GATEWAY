01370e696ed5f531926750c22d2d779c1619d947
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-04-10 23:30:10 -0400

    Added configuration in application.properties

--- src/main/java/com/williamfeliciano/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/williamfeliciano/apigateway/ApiGatewayApplication.java
@@ -3,11 +3,12 @@ package com.williamfeliciano.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
 }
